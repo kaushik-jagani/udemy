df3a2be39f42806fa64f08a322bf9c24d63ca7bc
kaushik jagani <kaushikjagani26@gmail.com>
2023-02-25 14:29:02 +0530

    complete one part of learning :)

--- api/index.js
+++ api/index.js
@@ -1,28 +1,58 @@
-const http =require('http');
-const fs = require('fs');
-const url =require('url');
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const slugify = require("slugify");
+const replaceTemplate = require("./modules/replaceTemplate");
+//functions
 
 //top level code execution once in allover program
-const data =  fs.readFileSync('./data.json','utf-8');
-const dataObj =JSON.parse(data);
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 
+const data = fs.readFileSync("./data.json", "utf-8");
+const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 
-const server = http.createServer((req,res)=>{
-    const pathname =req.url;
+console.log(slugs);
 
-    if(pathname === '/about'){
-        res.end('this is home about');
-    }else if(pathname === '/api'){
-      
-        res.writeHead(200,{'Content-type':'application/json'});
-        res.end(data);
-             
-    }else{
-        res.end('hello welcome to the local web server');
-    }
-})
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
 
-server.listen(4000,()=>{
-    console.log('local server start successfully .....');
-})
+  if (pathname === "/overview" || pathname === "/") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardHtml = dataObj.map((el) => replaceTemplate(tempCard, el));
+
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardHtml);
+    res.end(output);
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found </h1>");
+  }
+});
+
+server.listen(4000, () => {
+  console.log("local server start successfully .....");
+});
