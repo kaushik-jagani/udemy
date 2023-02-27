f7974991dd50d421ed947d4206354af9eea37c3a
kaushik jagani <kaushikjagani26@gmail.com>
2023-02-27 23:19:56 +0530

    update index file and async await concepts learning

--- async_await/index.js
+++ async_await/index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const { toUnicode } = require("punycode");
 const superagent = require("superagent");
 
 const readFilePro = (file) => {
@@ -25,21 +26,72 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
-  .then((data) => {
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
-    console.log(res.body.message, res.body.message);
-    return writeFilePro("dog-img2.txt", res.body.message);
-  })
-  .then(() => {
-    console.log("dog image is saved in text file\n");
-  })
-  .catch((err) => {
-    console.log(err.message + " error ocuured");
-  });
 
+//:::--- using async await
+//-> handle multiple promise
+
+const getDogPic = async ()=>{
+  try{
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Bread : ${data}`);
+  
+    const res1 = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+
+    const res2 = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+
+    const res3 = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+
+    const all1 =await Promise.all([res1,res2,res3]);
+    const imgs = all1.map(el =>
+      el.body.message);
+    console.log(imgs);
+  
+    await writeFilePro("dog-img2.txt", imgs.join('\n'));
+
+    console.log("dog image is saved in text file\n");
+  }catch(err){
+    console.log(err);
+  }
+
+}
+
+//->handle single promise
+
+// const getDogPic = async ()=>{
+//   try{
+//     const data = await readFilePro(`${__dirname}/dog.txt`);
+//     console.log(`Bread : ${data}`);
+  
+//     const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//     console.log(res.body.message);
+  
+//     await writeFilePro("dog-img2.txt", res.body.message);
+//     console.log("dog image is saved in text file\n");
+//   }catch(err){
+//     console.log(err);
+//   }
+
+// }
+ 
+getDogPic();
+
+// --: using promises
+
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body.message, res.body.message);
+//     return writeFilePro("dog-img2.txt", res.body.message);
+//   })
+//   .then(() => {
+//     console.log("dog image is saved in text file\n");
+//   })
+//   .catch((err) => {
+//     console.log(err.message + " error ocuured");
+//   });
+ 
 
    
 // fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
