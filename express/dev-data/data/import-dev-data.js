f9cb9345f6a2bd777444c75447b3b28d4f11ba49
kaushik jagani <kaushik.200410116078@gmail.com>
2023-04-01 17:30:26 +0530

    refrence modling with mongoose in tour model

--- express/dev-data/data/import-dev-data.js
+++ express/dev-data/data/import-dev-data.js
@@ -18,7 +18,7 @@ mongoose.connect(DB,{
 
 //read JSON file
 
-const tours =JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`,'utf-8'));
+const tours =JSON.parse(fs.readFileSync(`${__dirname}/tours.json`,'utf-8'));
 
 //IMPORT DATA DB
 
--- express/models/tourModel.js
+++ express/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -77,7 +78,36 @@ const tourSchema = new mongoose.Schema(
     secretTour: {
       type: Boolean,
       default: false
-    }
+    },
+    startLocation:{
+      type:{
+        type:String,
+        default:'Point',
+        enum:['Point']
+      },
+      coordinates:[Number],
+      address:String,
+      description:String
+    },
+    locations:[
+      {
+        type:{
+          type:String,
+          default:'Point',
+          enum:['Point']
+        },
+      coordinates:[Number],
+      address:String,
+      description:String,
+      day:Number
+      }
+    ],
+    guides:[
+      {
+        type:mongoose.Schema.ObjectId,
+        ref:'User'
+      }
+    ]
   },
   {
     toJSON: { virtuals: true },
@@ -94,6 +124,13 @@ tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
+
+// tourSchema.pre('save', async function(next) {
+//   const guidesPromises=this.guides.map(async id =>
+//     await User.findById(id));
+//   this.guides=await Promise.all(guidesPromises); 
+//   next();
+// });
 
 // tourSchema.pre('save', function(next) {
 //   console.log('Will save document...');
