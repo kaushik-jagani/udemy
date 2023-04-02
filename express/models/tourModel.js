33b733b8e823180c073376d5c3e5a93819a37bca
kaushik jagani <kaushik.200410116078@gmail.com>
2023-04-02 12:00:29 +0530

    populate function using middleware in tourmodel

--- express/models/tourModel.js
+++ express/models/tourModel.js
@@ -104,7 +104,7 @@ const tourSchema = new mongoose.Schema(
     ],
     guides:[
       {
-        type:mongoose.Schema.ObjectId,
+        type:mongoose.Schema.Types.ObjectId, 
         ref:'User'
       }
     ]
@@ -151,10 +151,19 @@ tourSchema.pre(/^find/, function(next) {
   next();
 });
 
+tourSchema.pre(/^find/,function(next){
+  this.populate({
+    path:'guides',
+    select:'-__v -passwordChangedAt'
+  });
+  next();
+})
+
 tourSchema.post(/^find/, function(docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   next();
 });
+
 
 // AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function(next) {
