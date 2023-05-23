3d4f570e6272b0081a49107cd5acb2e8b31a5a7c
kaushik jagani <kaushik.200410116078@gmail.com>
2023-05-23 17:30:44 +0530

    learn about populate 11 series of video

--- express/controllers/reviewController.js
+++ express/controllers/reviewController.js
@@ -6,9 +6,9 @@ exports.getAllreviews =catchAsync(async(req,res,next)=>{
 
     res.status(200).json({
         status:'success',
-        results:Review.length,
+        results:reviwes.length,
         data:{
-            reviews
+            reviwes
         }
     });
 });
@@ -16,7 +16,10 @@ exports.getAllreviews =catchAsync(async(req,res,next)=>{
 exports.createReview = catchAsync(async (req,res,next)=>{
     const newReview = await Review.create(req.body);
 
-    res.status(200).json({
-        
+    res.status(201).json({
+        status :'Success',
+        dat:{
+            review : newReview 
+        }
     })
 })
--- express/controllers/tourController.js
+++ express/controllers/tourController.js
@@ -28,8 +28,9 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
   });
 });
 
+//POPULATE CREATE A NEW QUERY   
 exports.getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id);
+  const tour = await Tour.findById(req.params.id).populate('reviews');
   // Tour.findOne({ _id: req.params.id })
 
   if (!tour) {
