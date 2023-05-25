844eec501c6abc0f6ef8808e58a460ed643313e0
kaushik jagani <kaushik.200410116078@gmail.com>
2023-05-25 18:08:14 +0530

    remove duplicate review while commenting.

--- express/controllers/handlerFactory.js
+++ express/controllers/handlerFactory.js
@@ -72,7 +72,9 @@ exports.getAll = Model => catchAsync(async (req, res, next) => {
     .sort()
     .limitFields()
     .paginate();
+  // const doc = await features.query.explain();
   const doc = await features.query;
+
 
   // SEND RESPONSE
   res.status(200).json({
--- express/models/tourModel.js
+++ express/models/tourModel.js
@@ -28,7 +28,7 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a difficulty'],
       enum: {
         values: ['easy', 'medium', 'difficult'],
-        message: 'Difficulty is either: easy, medium, difficult',
+        message: 'Difficulty is either: easy, medium, difficult'
       },
     },
     ratingsAverage: {
@@ -36,6 +36,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
       max: [5, 'Rating must be below 5.0'],
+      set: val => Math.round(val*10)/10
     },
     ratingsQuantity: {
       type: Number,
@@ -120,6 +121,12 @@ const tourSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   }
 );
+
+// tourSchema.index({price:1});
+
+//->compound index
+tourSchema.index({price:1,ratingsAverage : -1});
+tourSchema.index({slug:1}); 
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
