f710ab5a6ad454059fe626367b149c1ff0f5ae48
kaushik jagani <kaushik.200410116078@gmail.com>
2023-05-27 12:08:04 +0530

    hii

--- express/app2.js
+++ express/app2.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -5,15 +6,25 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
+
 
 
 const app = express();
+
+//setting template engine.
+app.set('view engine','pug');
+app.set('views',path.join(__dirname,'views'));
+
+//using path join for server root
+app.use(express.static(path.join(__dirname,'public')));
 
 // 1) GLOBAL MIDDLEWARES
 // Set security HTTP headers
@@ -34,6 +45,8 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -56,7 +69,10 @@ app.use(
 );
 
 // Serving static files
-app.use(express.static(`${__dirname}/public`));
+// app.use(express.static(`${__dirname}/public`));
+
+
+
 
 // Test middleware
 app.use((req, res, next) => {
@@ -66,9 +82,13 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
+
+
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
--- express/app3.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.static(`${__dirname}/public/overview.html`));
-
-app.listen(2411, () => {
-    console.log(`app is running ${2411}`);
-  });
